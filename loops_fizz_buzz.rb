c177cb1360e390c9ffa1fcc1ebafc692f744cd08
TableWaterFlower <charles@redwoodplains.com>
2022-07-20 22:28:55 +0000

    Completed loops_fizz_buzz Used while loop instead of times method.

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -14,18 +14,21 @@
 
 # end
 
+
+
 counter = 1
-
 while counter <= 100
-  if counter%3 == 0
-    puts "Fizz"
-  elsif counter%5 ==0 
-    puts "Buzz"
-  elsif counter%3==0 && counter%5==0
+  # if counter% 3 == 0 && counter% 5 == 0
+    if counter % 15 == 0
     puts "FizzBuzz"
-
+  elsif counter % 5 == 0 
+    puts "Buzz"
+  elsif counter % 3 == 0
+    puts "Fizz"
+  else 
+    puts counter
   end 
+
   counter = counter+1
-  puts counter
 
   end
